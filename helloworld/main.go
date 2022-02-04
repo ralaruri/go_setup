285b50e3bea3b0b302c35d3af6fd2248378ea5ac
Ramzi A <ramzi.alaruri@gmail.com>
2022-02-03 21:53:57 -0500

    added a basic introduction to go clique hello world

---
+++ helloworld/main.go
@@ -0,0 +1,11 @@
+// indicator this the main package of the executables we are generating
+package main
+
+// we are importing a package
+import "fmt"
+
+// Starting point of program
+func main() {
+	// main instruction to print to the console
+	fmt.Println("Hello World")
+}
